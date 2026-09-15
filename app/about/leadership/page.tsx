92121e824a4e91e39578d411d7cd384e0a469612
saarthak3007-creator <saarthak3007@gmail.com>
2026-09-15 18:34:34 -0500

    Update page.tsx

--- app/about/leadership/page.tsx
+++ app/about/leadership/page.tsx
@@ -2,7 +2,7 @@ const leadershipMembers = [
   { name: "Benny Winer", role: "President", image: "/images/team/leadership/benny-winer.jpg" },
   { name: "Jamane Tong", role: "Vice President", image: "/images/team/leadership/jamane-tong.jpg" },
   { name: "Agnes Tamatekou", role: "Treasurer", image: "/images/team/leadership/agnes-tamatekou.jpg" },
-  { name: "Saarthak Jain", role: "Sponsorship Coordinator", image: "/images/team/leadership/anonpf.png" },
+  { name: "Saarthak Jain", role: "Sponsorship Coordinator", image: "/images/team/leadership/sj.jpg" },
   { name: "Emma Zheng", role: "Communications Director", image: "/images/team/leadership/Emma.jpg" },
   { name: "Annika Lao", role: "Marketing Director", image: "/images/team/leadership/annika.jpg" },
   { name: "Matthew Willis", role: "Membership Director", image: "/images/team/leadership/Matt.jpg" },
